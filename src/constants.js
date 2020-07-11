859fd00102f2b32b94660bfca83f018d33599c39
= <termer@protonmail.com>
2020-07-11 11:52:17 -0400

    update constants to us same domain

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const appName = 'TwineMedia'
-export const filesRoot = 'http://192.168.1.77:8090/file/'
-export const thumbsRoot = 'http://192.168.1.77:8090/thumbnail/'
-export const apiRoot = 'http://192.168.1.77:8090/api/v1/'
-export const eventBusUrl = 'http://192.168.1.77:8090/eventbus'
+export const filesRoot = '/file/'
+export const thumbsRoot = '/thumbnail/'
+export const apiRoot = '/api/v1/'
+export const eventBusUrl = '/eventbus'
 export const listTypes = ['Standard', 'Automatically Populated']
 export const listVisibilities = ['Private', 'Public']
