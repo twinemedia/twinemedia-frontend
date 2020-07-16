f7594a3371b24b9bd8b6e839195e9097a2c185de
= <termer@protonmail.com>
2020-07-16 11:51:13 -0400

    make media settings more intuitive with dropdowns

--- src/components/MediaSettingsEditor.vue
+++ src/components/MediaSettingsEditor.vue
@@ -18,30 +18,63 @@
             </template>
         </select>
         <template v-if="type == 'video'">
-            <p>Frame rate (Use <code>-1</code> to keep original value)</p>
-            <label>Frame rate: </label>
-            <input placeholder="Frame rate (e.g. 30)" min="-1" max="512" type="number" v-model="frame_rate" @change="updateValue()" :disabled="disabled">
+            <p>Frame rate</p>
+            <select v-model="originalFramerate" @change="updateFramerate()">
+                <option value="true">Keep original frame rate</option>
+                <option value="false">Use custom frame rate:</option>
+            </select>
+            <template v-if="originalFramerate == 'false'">
+                <label></label>
+                <input placeholder="Frame rate (e.g. 30)" min="-1" max="512" type="number" v-model="frame_rate" @change="updateValue()" :disabled="disabled">
+            </template>
             <br><br>
         </template>
-        <p>Bitrate (Use <code>-1</code> to keep original value)</p>
-        <label>Audio Bitrate:</label>
-        <input placeholder="Audio Bitrate (e.g. 320)" min="-1" max="51200" type="number" v-model="audio_bitrate" @change="updateValue()" :disabled="disabled" /> kB/s
+        <p>Bitrate</p>
+        <select v-model="originalAudioBitrate" @change="updateAudioBitrate()">
+            <option value="true">Use default encoder audio bitrate</option>
+            <option value="false">Use custom audio bitrate:</option>
+        </select>
+        <template v-if="originalAudioBitrate == 'false'">
+            <label></label>
+            <input placeholder="Audio Bitrate (e.g. 320)" min="-1" max="51200" type="number" v-model="audio_bitrate" @change="updateValue()" :disabled="disabled" /> kB/s
+        </template>
         <br><br>
         <template v-if="type == 'video'">
-            <label>Video Bitrate:</label>
-            <input placeholder="Video Bitrate (e.g. 2048)" min="-1" max="51200" type="number" v-model="video_bitrate" @change="updateValue()" :disabled="disabled" /> kB/s
+            <select v-model="originalVideoBitrate" @change="updateValue()">
+                <option value="true">Use default encoder video bitrate</option>
+                <option value="false">Use custom video bitrate:</option>
+            </select>
+            <template v-if="originalVideoBitrate == 'false'">
+                <label></label>
+                <input placeholder="Video Bitrate (e.g. 2048)" min="-1" max="51200" type="number" v-model="video_bitrate" @change="updateValue()" :disabled="disabled" /> kB/s
+            </template>
             <br><br>
         </template>
         <template v-if="type == 'video'">
-            <p>Video Dimensions (Use <code>-1</code> to keep aspect ratio of original video)</p>
-            <label>Width:</label>
-            <input placeholder="Width (e.g. 640)" min="-1" max="7680" type="number" v-model="width" @change="updateValue()" :disabled="disabled" />
+            <p>Video Dimensions</p>
+            <select v-model="originalWidth" @change="updateWidth()">
+                <option value="true">Maintain aspect ratio</option>
+                <option value="false">Use custom width:</option>
+            </select>
+            <template v-if="originalWidth == 'false'">
+                <label></label>
+                <input placeholder="Width (e.g. 640)" min="-1" max="7680" type="number" v-model="width" @change="updateValue()" :disabled="disabled" />
+            </template>
             <br><br>
-            <label>Height:</label>
-            <input placeholder="Height (e.g. 360)" min="-1" max="4320" type="number" v-model="height" @change="updateValue()" :disabled="disabled" />
+            <select v-model="originalHeight" @change="updateHeight()">
+                <option value="true">Maintain aspect ratio</option>
+                <option value="false">Use custom height:</option>
+            </select>
+            <template v-if="originalHeight == 'false'">
+                <label></label>
+                <input placeholder="Height (e.g. 360)" min="-1" max="4320" type="number" v-model="height" @change="updateValue()" :disabled="disabled" />
+            </template>
             <template v-if="thumbnail">
                 <br><br>
-                <img :src="thumbnail" :width="width" :height="height" alt="Thumbnail" />
+                <img :src="thumbnail" :style="{
+                    width: width == -1 ? 'auto' : width+'px',
+                    height: height == -1 ? 'auto' : height+'px'
+                }" alt="Video preview thumbnail" />
             </template>
         </template>
     </div>
@@ -65,10 +98,15 @@ export default {
     props: ['type', 'metadata', 'thumbnail', 'preset', 'disabled'],
     data() {
         return {
+            originalWidth: 'true',
             width: -1,
+            originalHeight: 'false',
             height: 360,
+            originalFramerate: 'false',
             frame_rate: 30,
+            originalAudioBitrate: 'false',
             audio_bitrate: 80,
+            originalVideoBitrate: 'false',
             video_bitrate: 512,
             extension: ''
         }
@@ -89,7 +127,59 @@ export default {
         this.updateValue()
     },
     methods: {
+        updateFramerate() {
+            if(this.originalFramerate+'' == 'false')
+                this.frame_rate = 30
+            else
+                this.frame_rate = -1
+
+            this.updateValue()
+        },
+        updateWidth() {
+            if(this.originalWidth+'' == 'false' && this.width == -1)
+                this.width = 480
+            else
+                this.width = -1
+
+            this.updateValue()
+        },
+        updateHeight() {
+            if(this.originalHeight+'' == 'false' && this.height == -1)
+                this.height = 360
+            else
+                this.height = -1
+
+            this.updateValue()
+        },
+        updateAudioBitrate() {
+            if(this.originalAudioBitrate+'' == 'false' && this.audio_bitrate == -1)
+                this.audio_bitrate = 80
+            else
+                this.audio_bitrate = -1
+
+            this.updateValue()
+        },
+        updateVidoeBitrate() {
+            if(this.originalVideoBitrate+'' == 'false' && this.video_bitrate == -1)
+                this.video_bitrate = 512
+            else
+                this.video_bitrate = -1
+
+            this.updateValue()
+        },
         updateValue() {
+            // Set original/custom value selectors
+            if(this.frame_rate == -1)
+                this.originalFramerate = true
+            if(this.width == -1)
+                this.originalWidth = true
+            if(this.height == -1)
+                this.originalHeight = true
+            if(this.audio_bitrate == -1)
+                this.originalAudioBitrate = true
+            if(this.video_bitrate == -1)
+                this.originalVideoBitrate = true
+
             if(this.type == 'video') {
                 this.$emit('input', {
                     frame_rate: this.frame_rate,
