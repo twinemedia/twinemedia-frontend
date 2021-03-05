119bf01a617195f77b5847ea2c6ba74b93753cd7
= <termer@protonmail.com>
2021-03-04 20:51:17 -0500

    adjust what's shown depending on permissions

--- src/components/FileListing.vue
+++ src/components/FileListing.vue
@@ -5,22 +5,33 @@
         <p>Size: {{ $root.formatSize(file.size) }}</p>
     </div>
     <div v-else-if="display == 'preview'" class="file-listing file-listing-preview">
-        <router-link :to="'/file/'+file.id">
-            <img v-if="file.thumbnail" :src="thumbsRoot+file.id" :title="file.name || file.filename" height="120" />
+        <router-link v-if="canView" :to="'/file/'+file.id">
+            <img v-if="file.thumbnail" :src="thumbsRoot+file.id" :title="file.name || file.filename" height="120">
             <img v-else src="../assets/files.png" height="120" />
             <br>
             <b :title="file.name || file.filename">{{ $root.limit(file.name || file.filename, 50) }}</b>
             <br>
             <i :title="file.filename">({{ $root.limit(file.filename, 50) }})</i>
         </router-link>
-        <p><a :href="filesRoot+file.id+'/'+$root.urlEncode(file.filename)">Download</a></p>
-        <template v-if="addable &&
-            ($root.hasPermission('lists.add') || $root.hasPermission('lists.remove'))
-            &&
-            (($root.hasPermission('files.view') && file.creator == $root.account.id)
-            ||
-            ($root.hasPermission('files.view.all') && file.creator != $root.account.id))
-        ">
+        <a v-else>
+            <img v-if="file.thumbnail" :src="thumbsRoot+file.id" :title="file.name || file.filename" height="120">
+            <img v-else src="../assets/files.png" height="120" />
+            <br>
+            <b :title="file.name || file.filename">{{ $root.limit(file.name || file.filename, 50) }}</b>
+            <br>
+            <i :title="file.filename">({{ $root.limit(file.filename, 50) }})</i>
+        </a>
+        <p><a target="_blank" :href="filesRoot+file.id+'/'+$root.urlEncode(file.filename)">
+            <template v-if="
+                (file.mime.startsWith('image/') || file.mime.startsWith('video/') || file.mime.startsWith('audio/'))
+                &&
+                !(($root.hasPermission('files.view') && file.creator == $root.account.id)
+                ||
+                ($root.hasPermission('files.view.all') && file.creator != $root.account.id))
+            ">View File</template>
+            <template v-else>Download</template>
+        </a></p>
+        <template v-if="addable && ($root.hasPermission('lists.add') || $root.hasPermission('lists.remove')) && canView">
             <a href="" @click.prevent="$root.openListAddDialog(file.id)">Add/remove to list</a>
         </template>
         <br v-else>
@@ -64,13 +75,7 @@
         </td>
         <template v-if="addable">
             <td>
-                <template v-if="
-                    ($root.hasPermission('lists.add') || $root.hasPermission('lists.remove'))
-                    &&
-                    (($root.hasPermission('files.view') && file.creator == $root.account.id)
-                    ||
-                    ($root.hasPermission('files.view.all') && file.creator != $root.account.id))
-                ">
+                <template v-if="($root.hasPermission('lists.add') || $root.hasPermission('lists.remove')) && canView">
                     <a href="" @click.prevent="$root.openListAddDialog(file.id)">Add/remove to list</a>
                 </template>
                 <i v-else>None</i>
@@ -112,7 +117,10 @@ export default {
     data() {
         return {
             filesRoot,
-            thumbsRoot
+            thumbsRoot,
+            canView: (this.$root.hasPermission('files.view') && this.file.creator == this.$root.account.id)
+                     ||
+                     (this.$root.hasPermission('files.view.all') && this.file.creator != this.$root.account.id)
         }
     }
 }
