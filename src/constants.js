e3b5a5450048a5737d36215518a366c4fcdfc2ee
= <termer@protonmail.com>
2021-07-27 21:32:39 -0400

    restore constants back to their original values

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 export const appName = 'TwineMedia'
-export const filesRoot = 'http://localhost:8090/download/'
-export const thumbsRoot = 'http://localhost:8090/thumbnail/'
-export const apiRoot = 'http://localhost:8090/api/v1/'
-export const eventBusUrl = 'http://localhost:8090/eventbus'
+export const filesRoot = '/download/'
+export const thumbsRoot = '/thumbnail/'
+export const apiRoot = '/api/v1/'
+export const eventBusUrl = '/eventbus'
 export const listTypes = ['Standard', 'Automatically Populated']
 export const listVisibilities = ['Private', 'Public']
 export const version = '1.5.0'
